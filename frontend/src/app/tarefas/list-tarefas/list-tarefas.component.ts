315a6e1c677fdc6df2a4627327f1a684dc9c70dd
ReginaldoBorges <reginaldo.borges07@gmail.com>
2025-06-07 15:32:37 -0300

    Ajustes nos componentes e criação do list-tarefas.component

---
+++ frontend/src/app/tarefas/list-tarefas/list-tarefas.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-tarefas',
+  standalone: true,
+  templateUrl: './list-tarefas.component.html',
+  styleUrl: './list-tarefas.component.css',
+})
+export class ListTarefasComponent {}
